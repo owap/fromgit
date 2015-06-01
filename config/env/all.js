dce3597cba0677152495534f5a7f2ebf599324b6
Andrew J. Pierce <apierce@openwhere.com>
2015-06-01 19:19:11 +0000

    Remove unused and duplicate configuration values
    Remove the config values associated with unused Passport strategies, and
    move the "mailer" section to the "all.js" environment config file to
    keep the code DRY

--- config/env/all.js
+++ config/env/all.js
@@ -41,5 +41,15 @@ module.exports = {
 			'public/lib/angular-mocks/angular-mocks.js',
 			'public/modules/*/tests/*.js'
 		]
+	},
+	mailer: {
+		from: process.env.MAILER_FROM || 'MAILER_FROM',
+		options: {
+			service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+			auth: {
+				user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
+				pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+			}
+		}
 	}
 };
